ee3895b61c11b5f83d7db9c29bcd2ae15ff492ac
francisojeah <ojeahfrancis@gmail.com>
2023-04-06 07:36:25 +0100

    Primitive scalar typles

---
+++ rust_by_example/target/debug/deps/rust_by_example-7cdfe927c47c955c.d
@@ -0,0 +1,5 @@
+c:\Users\ojeah\Documents\PAU\YEAR 3\Semester 2\CSC 305 - Operating Systems II\csc305-2023\rust_by_example\target\debug\deps\rust_by_example-7cdfe927c47c955c.rmeta: src\main.rs
+
+c:\Users\ojeah\Documents\PAU\YEAR 3\Semester 2\CSC 305 - Operating Systems II\csc305-2023\rust_by_example\target\debug\deps\rust_by_example-7cdfe927c47c955c.d: src\main.rs
+
+src\main.rs:
---
+++ rust_by_example/target/debug/deps/rust_by_example-eaf6462635e5a572.d
@@ -0,0 +1,5 @@
+c:\Users\ojeah\Documents\PAU\YEAR 3\Semester 2\CSC 305 - Operating Systems II\csc305-2023\rust_by_example\target\debug\deps\rust_by_example-eaf6462635e5a572.rmeta: src\main.rs
+
+c:\Users\ojeah\Documents\PAU\YEAR 3\Semester 2\CSC 305 - Operating Systems II\csc305-2023\rust_by_example\target\debug\deps\rust_by_example-eaf6462635e5a572.d: src\main.rs
+
+src\main.rs:
