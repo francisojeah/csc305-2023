6b6a334b35c1a781746db659c50b89a1362040ef
francisojeah <ojeahfrancis@gmail.com>
2023-06-15 14:12:10 +0100

    os updates

--- os_with_bootloader/kernel_with_bootloader/src/main.rs
+++ os_with_bootloader/kernel_with_bootloader/src/main.rs
@@ -1,31 +1,23 @@
 #![no_std]
-
 #![no_main]
 
 use bootloader_api::config::Mapping;
 
 use x86_64::instructions::hlt;
 
-
 #[panic_handler]
 
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-
     loop {
-
         hlt();
-
     }
-
 }
- 
 
 //Use the entry_point macro to register the entry point function: bootloader_api::entry_point!(kernel_main)
 
 //optionally pass a custom config
 
 pub static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
-
     let mut config = bootloader_api::BootloaderConfig::new_default();
 
     config.mappings.physical_memory = Some(Mapping::Dynamic);
@@ -33,18 +25,20 @@ pub static BOOTLOADER_CONFIG: bootloader_api::BootloaderConfig = {
     config.kernel_stack_size = 100 * 1024; // 100 KiB
 
     config
-
 };
 
 bootloader_api::entry_point!(my_entry_point, config = &BOOTLOADER_CONFIG);
 
- 
-
 fn my_entry_point(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    //boot_info.framebuffer is our target for display.
+
+    let frame_buffer_info = boot_info.framebuffer.as_mut().unwrap().info();
+
+    let buffer = boot_info.framebuffer.as_mut().unwrap().buffer_mut();
+
+    // let mut frame_buffer_writer = 
 
     loop {
-
-        hlt();//stop x86_64 from being unnecessarily busy while looping
-
+        hlt(); //stop x86_64 from being unnecessarily busy while looping
     }
 }
